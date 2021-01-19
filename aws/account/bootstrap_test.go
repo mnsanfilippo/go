cacd6882e58702e8571508f1342de53dd5320cad
mariano.sanfilippo <mariano.sanfilippo@uala.com.ar>
2021-01-18 22:40:44 -0300

    added terragrunt plan and apply

--- aws/account/bootstrap_test.go
+++ aws/account/bootstrap_test.go
@@ -1,55 +1,21 @@
 package account
+
 import (
-	"fmt"
-	"github.com/go-git/go-git/v5/plumbing"
-	"io/ioutil"
 	log "github.com/sirupsen/logrus"
-	"os"
-	"strings"
 	"testing"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
-	copy "github.com/otiai10/copy"
 )
 var accountName = "bootstrap"
 var environment = "test"
 var terraformLiveDir string
 
-func gitClone(url string, branch string) string {
 
-	// Get the credentials to download the repository
-	username := os.Getenv("GITHUB_USERNAME")
-	token := os.Getenv("GITHUB_TOKEN")
-
-	// It will create a random directory name
-	dir, err := ioutil.TempDir(".", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Clone the given repository to the given directory
-	_, err = git.PlainClone(dir, false, &git.CloneOptions{
-		URL: url,
-		Auth: &http.BasicAuth{
-			Username: username, // anything except an empty string
-			Password: token,
-		},
-		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
-		Progress:      os.Stdout,
-	})
-	if err != nil {
-		log.Println(err)
-	}
-
-	return dir
-}
 
 func init(){
-	terraformLiveDir = gitClone("https://github.com/mnsanfilippo/terraform-live.git", "main")
+	terraformLiveDir = GitClone("https://github.com/mnsanfilippo/terraform-live.git", "main")
 }
 
 func TestCopyEnvironment(t *testing.T){
-	err := copy.Copy(terraformLiveDir + "/master/us-east-1/dev", terraformLiveDir +  "/master/us-east-1/" + environment)
+	err := CopyEnvironment(terraformLiveDir,environment)
 	if err != nil {
 		log.Error(err)
 		t.Fail()
@@ -58,24 +24,41 @@ func TestCopyEnvironment(t *testing.T){
 
 func TestReplaceInputs(t *testing.T){
 	filename := terraformLiveDir +  "/master/us-east-1/" + environment + "/accounts/terragrunt.hcl"
-	input, err := ioutil.ReadFile( filename)
-	if err != nil {
-		log.Fatalln(err)
+
+	replaceInput := []ReplaceInput{
+		{
+			old: "account_name",
+			new: "  account_name = " + "\"" + environment + "\"",
+		},
+		{
+			old: "account_email",
+			new: "  account_email = " + "\"mnsanfilippo+" + environment + "@gmail.com\"",
+		},
 	}
 
-	lines := strings.Split(string(input), "\n")
-
-	for i, line := range lines {
-		if strings.Contains(line, "account_name") {
-			lines[i] = "  account_name = " + "\"" + environment + "\""
-		}
-		if strings.Contains(line, "account_email") {
-			lines[i] = "  account_email = " + "\"mnsanfilippo+" + environment + "@gmail.com\""
-		}
-	}
-	output := strings.Join(lines, "\n")
-	err = ioutil.WriteFile(filename, []byte(output), 0644)
+	err := ReplaceInputs(filename,replaceInput)
 	if err != nil {
-		log.Fatalln(err)
+		log.Error(err)
+		t.Fail()
+	}
+}
+
+func TestTerragruntPlan(t *testing.T){
+
+	workingDirectory := terraformLiveDir +  "/master/us-east-1/" + environment + "/accounts/"
+	err := TerragruntPlan(workingDirectory)
+	if err != nil {
+		log.Fatal(err)
+		t.Fail()
+	}
+}
+
+func TestTerragruntApply(t *testing.T){
+
+	workingDirectory := terraformLiveDir +  "/master/us-east-1/" + environment + "/accounts/"
+	err := TerragruntApply(workingDirectory)
+	if err != nil {
+		log.Fatal(err)
+		t.Fail()
 	}
 }
